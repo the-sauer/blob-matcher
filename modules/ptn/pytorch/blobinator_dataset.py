e3e4be528cd55cc8de3ba392e7a7f8c71846a36a
Hendrik Sauer <hendrik-sauer@mailbox.org>
2025-10-05 18:13:31 +0200

    Improve code style.

--- modules/ptn/pytorch/blobinator_dataset.py
+++ modules/ptn/pytorch/blobinator_dataset.py
@@ -2,7 +2,8 @@
 This module provides an extension to learn the features of the Blobinator calibration sheet.
 """
 
-from typing import Generator, Sequence, TypeAlias, TypedDict, Union
+from abc import ABC
+from typing import Sequence, TypeAlias
 
 import cv2 as cv
 import numpy as np
@@ -10,16 +11,10 @@ import pdf2image
 import torch
 
 
-class ImageWrapper(TypedDict):
-    img: np.ndarray
-    padUp: int
-    padLeft: int
-
-
 Keypoint: TypeAlias = tuple[np.ndarray, float, float]
 
 
-class BlobinatorDataset(torch.utils.data.Dataset):
+class BlobinatorDataset(torch.utils.data.Dataset, ABC):
     """
     This is the Blobinator dataset.
 
@@ -28,22 +23,28 @@ class BlobinatorDataset(torch.utils.data.Dataset):
 
     def __init__(self, cfg) -> None:
         self.cfg = cfg
-        blobs = np.array(pdf2image.convert_from_path(cfg.BLOBINATOR.BLOB_SHEET, grayscale=True)[0])
-        self.blob_sheet: np.ndarray = np.zeros((1, self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO))
+        blobs = np.array(
+            pdf2image.convert_from_path(cfg.BLOBINATOR.BLOB_SHEET, grayscale=True)[0]
+        )
+        self.blob_sheet: np.ndarray = np.zeros(
+            (1, self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+        )
         self.blob_sheet_pad_left: int = (cfg.TRAINING.PAD_TO - blobs.shape[0]) // 2
         self.blob_sheet_pad_up: int = (cfg.TRAINING.PAD_TO - blobs.shape[1]) // 2
         self.blob_sheet[
             :,
-            self.blob_sheet_pad_left:self.blob_sheet_pad_left+blobs.shape[0],
-            self.blob_sheet_pad_up: self.blob_sheet_pad_up + blobs.shape[1]
+            self.blob_sheet_pad_left : self.blob_sheet_pad_left + blobs.shape[0],
+            self.blob_sheet_pad_up : self.blob_sheet_pad_up + blobs.shape[1],
         ] = blobs
         self.keypoints: Sequence[Keypoint] = list(
             map(
                 lambda k: (
-                    np.array([
-                        k[0][0] * blobs.shape[0] + self.blob_sheet_pad_left,
-                        k[0][1] * blobs.shape[1] + self.blob_sheet_pad_up,
-                    ]),
+                    np.array(
+                        [
+                            k[0][0] * blobs.shape[0] + self.blob_sheet_pad_left,
+                            k[0][1] * blobs.shape[1] + self.blob_sheet_pad_up,
+                        ]
+                    ),
                     k[1],
                     k[2],
                 ),
@@ -55,8 +56,12 @@ class BlobinatorDataset(torch.utils.data.Dataset):
                 ],
             )
         )
-        self.backgrounds: np.ndarray = np.zeros(shape=(256, self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO))
-        self.homographies = [self.generate_homography() for _ in range(len(self.backgrounds))]
+        self.backgrounds: np.ndarray = np.zeros(
+            shape=(256, self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+        )
+        self.homographies = [
+            self.generate_homography() for _ in range(len(self.backgrounds))
+        ]
 
     def generate_homography(self) -> np.ndarray:
         """
@@ -71,8 +76,16 @@ class BlobinatorDataset(torch.utils.data.Dataset):
             shorter sidelength of the blobsheet.
         """
         # TODO: Use superpoints
-        scale = np.random.uniform(self.cfg.BLOBINATOR.MIN_SCALE, self.cfg.BLOBINATOR.MAX_SCALE)
-        rotation = np.random.uniform(-self.cfg.BLOBINATOR.MAX_ROTATION, self.cfg.BLOBINATOR.MAX_ROTATION) * np.pi / 180
+        scale = np.random.uniform(
+            self.cfg.BLOBINATOR.MIN_SCALE, self.cfg.BLOBINATOR.MAX_SCALE
+        )
+        rotation = (
+            np.random.uniform(
+                -self.cfg.BLOBINATOR.MAX_ROTATION, self.cfg.BLOBINATOR.MAX_ROTATION
+            )
+            * np.pi
+            / 180
+        )
         translation_x = np.random.uniform(
             0, self.backgrounds.shape[1] - self.blob_sheet.shape[0]
         )
@@ -111,7 +124,7 @@ class BlobinatorDataset(torch.utils.data.Dataset):
         )
         homography = cv.getPerspectiveTransform(unit_points, transformed_points)
         return homography
-    
+
     def normalize_keypoint(self, keypoint, into):
         loc, scale, rotation = keypoint
         normalized_x = 2 * loc[0] / into[0] - 1
@@ -122,11 +135,13 @@ class BlobinatorDataset(torch.utils.data.Dataset):
         """
         Warps the blobsheet into the background according to a homography.
         """
-        warped_blobs = np.zeros(shape=(1, self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO))
+        warped_blobs = np.zeros(
+            shape=(1, self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+        )
         warped_blobs[0, :, :] = cv.warpPerspective(
             self.blob_sheet[0, :, :],
             homography,
-            (self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+            (self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO),
         )
         return warped_blobs
 
@@ -134,13 +149,16 @@ class BlobinatorDataset(torch.utils.data.Dataset):
         """
         Maps the list of keypoints according to the homography.
         """
+
         def map_keypoint(k: Keypoint) -> Keypoint:
             location, scale, rotation = k
             x = location[0]
             y = location[1]
             mapped_location = homography @ np.array([x, y, 1])
             mapped_location = mapped_location[:2] / mapped_location[2]
-            second_location = homography @ np.array([x - np.sin(rotation) * scale, y + np.cos(rotation) * scale, 1])
+            second_location = homography @ np.array(
+                [x - np.sin(rotation) * scale, y + np.cos(rotation) * scale, 1]
+            )
             second_location = second_location[:2] / second_location[2]
             new_rotation = np.arccos(
                 np.dot(second_location - mapped_location, np.array([0, 1]))
@@ -153,10 +171,8 @@ class BlobinatorDataset(torch.utils.data.Dataset):
 
         return list(map(map_keypoint, self.keypoints))
 
-class BlobinatorTrainDataset(BlobinatorDataset):
-    def __init__(self, cfg):
-        super().__init__(cfg)
 
+class BlobinatorTrainDataset(BlobinatorDataset):
     def __getitem__(self, index):
         background = self.backgrounds[index // len(self.keypoints)]
         transform = self.homographies[index // len(self.keypoints)]
@@ -164,40 +180,36 @@ class BlobinatorTrainDataset(BlobinatorDataset):
         mapped_keypoints = self.map_keypoints(transform)
         k = self.keypoints[index % len(self.keypoints)]
         k_mapped = mapped_keypoints[index % len(self.keypoints)]
-        # TODO: Normalize
-        normalized_k = self.normalize_keypoint(k, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO))
-        normalized_k_mapped = self.normalize_keypoint(k_mapped, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO))
+        normalized_k = self.normalize_keypoint(
+            k, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+        )
+        normalized_k_mapped = self.normalize_keypoint(
+            k_mapped, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+        )
         return (
             {
                 "img": self.blob_sheet,
                 "padLeft": self.blob_sheet_pad_left,
-                "padUp": self.blob_sheet_pad_up
+                "padUp": self.blob_sheet_pad_up,
             },
-            {
-                "img": mapped_image,
-                "padLeft": 0,
-                "padUp": 0
-            },
+            {"img": mapped_image, "padLeft": 0, "padUp": 0},
             normalized_k,
             normalized_k_mapped,
             "img0000",
-            f"img{(index % 4)+1:04}",
+            f"img{(index % 4) + 1:04}",
             1.0,
             0.0,
         )
-        
+
     def __len__(self):
         return len(self.backgrounds) * len(self.keypoints)
-    
+
     def get_pairs(self, _):
         # Do nothing for now. This method could load new background images or so.
         pass
 
 
 class BlobinatorTestDataset(BlobinatorDataset):
-    def __init__(self,cfg):
-        super().__init__(cfg)
-
     def __getitem__(self, index):
         background = self.backgrounds[index // (len(self.keypoints) ** 2)]
         transform = self.homographies[index // (len(self.keypoints) ** 2)]
@@ -208,30 +220,30 @@ class BlobinatorTestDataset(BlobinatorDataset):
         k = self.keypoints[keypoint_1_idx]
         k_mapped = mapped_keypoints[keypoint_2_idx]
         # TODO: Normalize
-        normalized_k = self.normalize_keypoint(k, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO))
-        normalized_k_mapped = self.normalize_keypoint(k_mapped, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO))
+        normalized_k = self.normalize_keypoint(
+            k, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+        )
+        normalized_k_mapped = self.normalize_keypoint(
+            k_mapped, into=(self.cfg.TRAINING.PAD_TO, self.cfg.TRAINING.PAD_TO)
+        )
         return (
             {
                 "img": self.blob_sheet,
                 "padLeft": self.blob_sheet_pad_left,
-                "padUp": self.blob_sheet_pad_up
+                "padUp": self.blob_sheet_pad_up,
             },
-            {
-                "img": mapped_image,
-                "padLeft": 0,
-                "padUp": 0
-            },
+            {"img": mapped_image, "padLeft": 0, "padUp": 0},
             normalized_k,
             normalized_k_mapped,
             "img0000",
-            f"img{(index % 4)+1:04}",
+            f"img{(index % 4) + 1:04}",
             k,
             k_mapped,
             1.0,
             1,
             0,
-            1 if keypoint_1_idx == keypoint_2_idx else 0
+            1 if keypoint_1_idx == keypoint_2_idx else 0,
         )
-        
+
     def __len__(self):
         return len(self.backgrounds) * len(self.keypoints) * len(self.keypoints)
