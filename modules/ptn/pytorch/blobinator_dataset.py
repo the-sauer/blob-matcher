94106b09ee556ab0bd4f04663b51d62373eb0eee
Hendrik Sauer <hendrik-sauer@mailbox.org>
2025-10-12 17:38:20 +0200

    Improve style

--- modules/ptn/pytorch/blobinator_dataset.py
+++ modules/ptn/pytorch/blobinator_dataset.py
@@ -2,8 +2,6 @@
 This module provides an extension to learn the features of the Blobinator calibration sheet.
 """
 
-#from BlobBoards import blob_pattern
-
 from abc import ABC
 from itertools import chain, repeat
 import json
@@ -11,22 +9,30 @@ import logging
 import os
 from typing import Any, Generator, NotRequired, Sequence, TypeAlias, TypedDict
 
+
 import cv2 as cv
-from matplotlib import pyplot as plt
 import numpy as np
 import pdf2image
 import skimage
 import torch
 
 
-Ellipse: TypeAlias = tuple[np.ndarray, tuple[float, float], float]
-Keypoint: TypeAlias = tuple[np.ndarray, float, float]
+Ellipse: TypeAlias = tuple[np.typing.NDArray, tuple[float, float], float]
+
+
+Keypoint: TypeAlias = tuple[np.typing.NDArray, float, float]
+
+
 class BlobParam(TypedDict):
     value: float
     unit: str
+
+
 class IsoBlob(TypedDict):
     center: list[BlobParam]
     σ: BlobParam
+
+
 class BoardBundle(TypedDict):
     preamble: NotRequired[Any]
     blobs: list[IsoBlob]
@@ -36,12 +42,13 @@ class BlobinatorDataset(ABC):
     """
     This is an abstract base class for Blobinator datasets.
 
-    It warps the Blobinator sheet and its keypoints into a number of background images via randomly generated homographies.
+    It warps the Blobinator sheet and its keypoints into a number of background images via randomly generated
+    homographies.
     """
 
     def __init__(self, cfg) -> None:
         self.cfg = cfg
-        
+
         # res = blob_pattern(
         #     cfg.BLOBINATOR.PATTERN_HEIGHT,
         #     cfg.BLOBINATOR.PATTERN_WIDTH,
@@ -51,15 +58,19 @@ class BlobinatorDataset(ABC):
         # )
         # files = res["files_created"]
         files = ["blob_pattern_1DA.json", "blob_pattern_1DA.png"]
-        blob_metadata: BoardBundle = { "blobs": [] }
-        self.blobs = np.zeros(shape=(cfg.BLOBINATOR.PATTERN_WIDTH, cfg.BLOBINATOR.PATTERN_HEIGHT))
+        blob_metadata: BoardBundle = {"blobs": []}
+        self.blobs: np.typing.NDArray = np.zeros(shape=(cfg.BLOBINATOR.PATTERN_WIDTH, cfg.BLOBINATOR.PATTERN_HEIGHT))
         for file in files:
             full_path = os.path.join(cfg.BLOBINATOR.BOARD_DIR, file)
             if file.endswith(".json"):
                 with open(full_path) as f:
                     blob_metadata = json.loads(f.read())
             elif file.endswith(".png"):
-                self.blobs = cv.imread(full_path, cv.IMREAD_GRAYSCALE)
+                read_blobs = cv.imread(full_path, cv.IMREAD_GRAYSCALE)
+                if read_blobs is not None:
+                    self.blobs = read_blobs / 255
+                else:
+                    logging.error("Could not read blob board")
             elif file.endswith(".pdf"):
                 self.blobs = np.array(pdf2image.convert_from_path(full_path, grayscale=True)[0])
             else:
@@ -69,12 +80,14 @@ class BlobinatorDataset(ABC):
             lambda k: (np.array([k["center"][0]["value"] - 1, k["center"][1]["value"] - 1]), k["σ"]["value"], 0),
             blob_metadata["blobs"]
         ))
-        self.backgrounds: np.ndarray = np.zeros(
+
+        self.backgrounds = np.zeros(
             shape=(
                 len(list(filter(lambda x: x.endswith(".jpg"), os.listdir((cfg.BLOBINATOR.BACKGROUND_DIR))))),
                 self.cfg.TRAINING.PAD_TO,
                 self.cfg.TRAINING.PAD_TO
-            )
+            ),
+            dtype=np.float32
         )
         for i, img_path in enumerate(filter(lambda x: x.endswith(".jpg"), os.listdir(cfg.BLOBINATOR.BACKGROUND_DIR))):
             img = cv.imread(os.path.join(cfg.BLOBINATOR.BACKGROUND_DIR, img_path), cv.IMREAD_GRAYSCALE)
@@ -89,7 +102,7 @@ class BlobinatorDataset(ABC):
                 crop1 = (img.shape[1] - img.shape[0]) // 2
                 crop2 = crop1 if 2 * crop1 + img.shape[0] == img.shape[1] else crop1 + 1
                 img = img[:, crop1:-crop2]
-            self.backgrounds[i, :, :] = cv.resize(img, dsize=(cfg.TRAINING.PAD_TO, cfg.TRAINING.PAD_TO))                
+            self.backgrounds[i, :, :] = cv.resize(img, dsize=(cfg.TRAINING.PAD_TO, cfg.TRAINING.PAD_TO)) / 255
 
         self.homographies = [
             self.sample_homography(
@@ -119,10 +132,11 @@ class BlobinatorDataset(ABC):
         max_angle=np.pi / 2,
         allow_artifacts=False,
         translation_overflow=0.0,
-    ):
+    ) -> np.typing.NDArray:
         """Sample a random valid homography.
 
-        **Note:** This function is an adapted version from [SuperPoints](github.com/rpautrat/SuperPoint) homography sampling.
+        **Note:** This function is an adapted version from [SuperPoints](github.com/rpautrat/SuperPoint) homography
+        sampling.
 
         Computes the homography transformation between a random patch in the original image and a warped projection
         with the same image size. It maps the output point (warped patch) to a transformed input point (original
@@ -153,6 +167,7 @@ class BlobinatorDataset(ABC):
             result = np.random.normal(loc, scale, shape)
             while any(result < loc - 2 * scale) or any(result > loc + 2 * scale):
                 result = np.random.normal(loc, scale, shape)
+                logging.debug("Recalculated truncated normal")
             return result
 
         # Corners of the output image
@@ -182,7 +197,7 @@ class BlobinatorDataset(ABC):
         if scaling:
             scales = np.concatenate([[1.0], _truncated_normal(1, scaling_amplitude / 2, (n_scales,))], 0)
             center = np.mean(pts2, axis=0, keepdims=True)
-            scaled = (np.expand_dims(pts2 - center, axis=0) * np.expand_dims(np.expand_dims(scales, 1), 1) + center)
+            scaled = np.expand_dims(pts2 - center, axis=0) * np.expand_dims(np.expand_dims(scales, 1), 1) + center
             if allow_artifacts:
                 valid = np.arange(start=1, stop=n_scales + 1)  # all scales are valid except scale=1
             else:
@@ -223,7 +238,7 @@ class BlobinatorDataset(ABC):
             pts2 = rotated[idx]
 
         # Rescale to actual size
-        original_shape = original_shape[::-1]#.astype(np.float32)  # different convention [y, x]
+        original_shape = tuple(map(float, original_shape[::-1]))  # different convention [y, x]
         pts1 = pts1 * np.expand_dims(original_shape, axis=0)
         pts2 = pts2 * np.expand_dims(original_shape, axis=0)
 
@@ -241,24 +256,22 @@ class BlobinatorDataset(ABC):
         homography[1, :] = flat_homography[0][3:6]
         homography[2, :2] = flat_homography[0][6:]
         correct_translation = np.identity(3)
-        correct_translation[0,2] = (patch_shape[0] - original_shape[0]) // 2
-        correct_translation[1,2] = (patch_shape[1] - original_shape[1]) // 2
+        correct_translation[0, 2] = (patch_shape[0] - original_shape[0]) // 2
+        correct_translation[1, 2] = (patch_shape[1] - original_shape[1]) // 2
         return correct_translation @ homography
 
-    def map_blobs(self, background: np.ndarray, homography: np.ndarray) -> np.ndarray:
+    def map_blobs(self, background: np.typing.NDArray, homography: np.typing.NDArray) -> np.typing.NDArray:
         """
         Warps the blobsheet into the background according to a homography.
 
         The blobboard is taken from `self`.
 
-        Parameters
-        ----------
-        background : A 2-D array containing the image data for the background.
-        homography : A valid homography with shape (3,3) that describes the transformation from the blobboard into
-                the image.
+        Argsument:
+            background: A 2-D array containing the image data for the background.
+            homography: A valid homography with shape (3,3) that describes the transformation from the blobboard into
+            the image.
 
-        Returns
-        -------
+        Returns:
             An image with the blobsheet mapped in front of a background.
         """
         warped_blobs = cv.warpPerspective(
@@ -270,8 +283,8 @@ class BlobinatorDataset(ABC):
         )
         warped_blobs.reshape(background.shape)
         return warped_blobs
-    
-    def keypoint_to_mapped_conic(self, homography: np.ndarray, k: Keypoint) -> np.ndarray:
+
+    def keypoint_to_mapped_conic(self, homography: np.typing.NDArray, k: Keypoint) -> np.typing.NDArray:
         """
         Transforms a keypoint into a conic section and warps it via a homography.
 
@@ -279,55 +292,44 @@ class BlobinatorDataset(ABC):
         section, it can be transformed with a homography. We expect to obtain an elliptical conic section as a result,
         since the perspective change of the used homographies are not that large.
 
-        Parameters
-        ----------
-        homography : The homography with which the keypoint will be mapped.
-        k : The keypoint.
+        Arguments:
+            homography: The homography with which the keypoint will be mapped.
+            k: The keypoint.
 
-        Returns
-        -------
+        Returns:
             An array of shape (3,3) representing the conic section of the mapped keypoint.
         """
         location, scale, _ = k
         x = location[0]
         y = location[1]
-        conic = np.array(
-            [[1, 0, -x],
-            [0, 1, -y],
-            [-x, -y, x**2 + y**2 - scale**2]],
-            dtype=float
-        )
+        conic = np.array([[1, 0, -x], [0, 1, -y], [-x, -y, x**2 + y**2 - scale**2]], dtype=float)
         mapped_conic = np.linalg.inv(homography).transpose() @ conic @ np.linalg.inv(homography)
         mapped_conic = (mapped_conic + mapped_conic.transpose()) / 2
         return mapped_conic
-    
-    def conic_to_ellipse(self, conic: np.ndarray) -> tuple[np.ndarray, tuple[float, float], float]:
+
+    def conic_to_ellipse(self, conic: np.typing.NDArray) -> tuple[np.typing.NDArray, tuple[float, float], float]:
         """
         Extracts ellipse parameters from a conic section.
 
-        Parameters
-        ----------
-        conic : A (3,3) array with the conic section.
+        Arguments:
+            conic: A (3,3) array with the conic section.
 
-        Returns
-        -------
+        Returns:
             A tuple consisting of
                 - a 2 entry array with the location,
                 - a tuple containing the semi-major axis and the semi-minor axis, and
                 - the angle of the semi-major axis in radians.
 
-        Raises
-        ------
-        AssertionError
-            When the conic does not represent a valid ellipse.
+        Raises:
+            AssertionError: When the conic does not represent a valid ellipse.
         """
-        location = -np.linalg.inv(conic[:2,:2] * 2) @ (conic[:2,2] * 2)
-        A = conic[0,0]
-        B = conic[0,1] * 2
-        C = conic[1,1]
-        D = conic[0,2] * 2
-        E = conic[1,2] * 2
-        F = conic[2,2]
+        location = -np.linalg.inv(conic[:2, :2] * 2) @ (conic[:2, 2] * 2)
+        A = conic[0, 0]
+        B = conic[0, 1] * 2
+        C = conic[1, 1]
+        D = conic[0, 2] * 2
+        E = conic[1, 2] * 2
+        F = conic[2, 2]
         x_0 = location[0]
         y_0 = location[1]
 
@@ -344,62 +346,50 @@ class BlobinatorDataset(ABC):
 
         return location, (semi_major_axis, semi_minor_axis), angle
 
-    def ellipse_to_affine(self, ellipse: Ellipse) -> np.ndarray:
+    def ellipse_to_affine(self, ellipse: Ellipse) -> np.typing.NDArray:
         """
         Finds an affine transformation that transforms the unit circle into the ellipse.
 
-        Parameters
-        ----------
+        Arguments:
             ellipse: The ellipse in which the unit circle should be transformed.
 
-        Returns
-        -------
+        Returns:
             An (3,3) array representing the affine transformation.
         """
         location, (semi_major_axis, semi_minor_axis), angle = ellipse
-        scale = np.diag([
-            semi_major_axis,# * self.cfg.BLOBINATOR.PATCH_SCALE_FACTOR,
-            semi_minor_axis,# * self.cfg.BLOBINATOR.PATCH_SCALE_FACTOR,
-            1
-        ])
+        scale = np.diag([semi_major_axis, semi_minor_axis, 1])
         rotation = np.identity(3)
-        rotation[:2,:] = cv.getRotationMatrix2D((0,0), -angle * 180 / np.pi, 1)
+        rotation[:2, :] = cv.getRotationMatrix2D((0, 0), -angle * 180 / np.pi, 1)
         translation = np.identity(3)
-        translation[:2,2] = location
+        translation[:2, 2] = location
         return translation @ rotation @ scale
-    
-    def ellipse_to_affine_inv(self, ellipse: Ellipse) -> np.ndarray:
+
+    def ellipse_to_affine_inv(self, ellipse: Ellipse) -> np.typing.NDArray:
         """
         Finds an affine transformation that transforms the ellipse into a unit circle.
 
-        Parameters
-        ----------
+        Parameters:
             ellipse: The ellipse which should be transformed into the unit circle .
 
-        Returns
-        -------
+        Returns:
             An (3,3) array representing the affine transformation.
         """
         location, (semi_major_axis, semi_minor_axis), angle = ellipse
-        scale = np.diag([
-            semi_major_axis,# * self.cfg.BLOBINATOR.PATCH_SCALE_FACTOR,
-            semi_minor_axis,# * self.cfg.BLOBINATOR.PATCH_SCALE_FACTOR,
-            1
-        ])
+        scale = np.diag([semi_major_axis, semi_minor_axis, 1])
         rotation = np.identity(3)
-        rotation[:2,:] = cv.getRotationMatrix2D((0,0), -angle * 180 / np.pi, 1)
+        rotation[:2, :] = cv.getRotationMatrix2D((0, 0), -angle * 180 / np.pi, 1)
         translation = np.identity(3)
-        translation[:2,2] = location
+        translation[:2, 2] = location
         affine = translation @ rotation @ scale
-        affine[:2,:] = cv.invertAffineTransform(affine[:2,:])
+        affine[:2, :] = cv.invertAffineTransform(affine[:2, :])
         return affine
-    
+
     def get_patch(self, img, A, pad_with=255):
         def backwards_map(cr):
             coords = np.empty_like(cr)
             for i in range(cr.shape[0]):
-                theta = cr[i,0] * 2 * np.pi / self.cfg.INPUT.IMAGE_SIZE
-                rho = cr[i,1] * np.log(self.cfg.BLOBINATOR.PATCH_SCALE_FACTOR) / self.cfg.INPUT.IMAGE_SIZE
+                theta = cr[i, 0] * 2 * np.pi / self.cfg.INPUT.IMAGE_SIZE
+                rho = cr[i, 1] * np.log(self.cfg.BLOBINATOR.PATCH_SCALE_FACTOR) / self.cfg.INPUT.IMAGE_SIZE
                 r = np.exp(rho)
                 coords[i] = (A @ np.array([r * np.cos(theta), r * np.sin(theta), 1]))[:2]
             return coords
@@ -415,17 +405,16 @@ class BlobinatorDataset(ABC):
 class BlobinatorTrainDataset(torch.utils.data.IterableDataset, BlobinatorDataset):
     def __iter__(self) -> Generator[
         tuple[
-            np.ndarray,
-            np.ndarray,
-            np.ndarray,
+            np.typing.NDArray,
+            np.typing.NDArray,
+            np.typing.NDArray,
             bool
         ],
         None,
         None
     ]:
         """
-        Yields
-        ------
+        Yields:
             A tuple consisting of
                 - the anchor patch,
                 - a positive patch (i.e.) the corresponding patch to the anchor patch in the warped image,
@@ -434,58 +423,67 @@ class BlobinatorTrainDataset(torch.utils.data.IterableDataset, BlobinatorDataset
         """
         for homography, background in zip(self.homographies, self.backgrounds):
             warped_image = self.map_blobs(background, homography)
-            cv.imwrite("warped_image.png", warped_image)
+            cv.imwrite("warped_image.png", (warped_image * 255).astype(np.uint8))
             garbage_keypoints: Sequence[Keypoint] = []  # TODO: Find garbage keypoints
             for keypoint, garbage_keypoint in zip(self.keypoints, chain(garbage_keypoints, repeat(None))):
                 # TODO: Log-polar transform
                 # TODO: Augmentation
                 anchor_patch_transform = self.ellipse_to_affine((keypoint[0], (keypoint[1], keypoint[1]), 0))
-                
+
                 anchor_patch = self.get_patch(self.blobs, anchor_patch_transform)
                 # TODO: Augmentation
-                positive_patch_transform = self.ellipse_to_affine(self.conic_to_ellipse(self.keypoint_to_mapped_conic(homography, keypoint)))
+                positive_patch_transform = self.ellipse_to_affine(self.conic_to_ellipse(self.keypoint_to_mapped_conic(
+                    homography,
+                    keypoint
+                )))
                 positive_patch = self.get_patch(warped_image, positive_patch_transform)
 
                 garbage_available = False
                 garbage_patch = np.zeros(shape=(1, self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE))
                 if garbage_keypoint is not None:
-                    garbage_patch_transform = self.ellipse_to_affine(self.conic_to_ellipse(self.keypoint_to_mapped_conic(homography, garbage_keypoint)))
+                    garbage_patch_transform = self.ellipse_to_affine(self.conic_to_ellipse(
+                        self.keypoint_to_mapped_conic(homography, garbage_keypoint)
+                    ))
                     garbage_patch = self.get_patch(warped_image, garbage_patch_transform)
                     garbage_available = True
-                
+
                 yield (
-                    anchor_patch.reshape(1, self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE).astype(np.float32) / 255,
-                    positive_patch.reshape(1, self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE).astype(np.float32) / 255,
-                    garbage_patch.reshape(1, self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE).astype(np.float32) / 255,
+                    anchor_patch.reshape(1, self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE),
+                    positive_patch.reshape(1, self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE),
+                    garbage_patch.reshape(1, self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE),
                     garbage_available
                 )
 
 
 class BlobinatorTestDataset(torch.utils.data.Dataset, BlobinatorDataset):
-    def __getitem__(self, index) -> tuple[np.ndarray, np.ndarray, float]:
+    def __getitem__(self, index) -> tuple[np.typing.NDArray, np.typing.NDArray, float]:
         # TODO: Implement
         return (
             np.zeros(shape=(self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE)),
             np.zeros(shape=(self.cfg.INPUT.IMAGE_SIZE, self.cfg.INPUT.IMAGE_SIZE)),
             1.
         )
-    
+
     def __len__(self):
         return 1
 
 
-if __name__ == "__main__":
+def main():
     import sys
     sys.path.insert(0, os.getcwd())
     from configs.defaults import _C as cfg
 
-    dir = "./data/patches"
+    directory = "./data/patches"
 
     dataset = BlobinatorTrainDataset(cfg)
 
     for i, (anchor_patch, positive_patch, garbage_patch, garbage_available) in zip(range(3), dataset):
-        os.makedirs(os.path.join(dir, f"{i:02}"), exist_ok=True)
-        cv.imwrite(os.path.join(dir, f"{i:02}", "anchor.png"), (anchor_patch[0] * 255).astype(np.uint8))
-        cv.imwrite(os.path.join(dir, f"{i:02}", "positive.png"), (positive_patch[0] * 255).astype(np.uint8))
+        os.makedirs(os.path.join(directory, f"{i:02}"), exist_ok=True)
+        cv.imwrite(os.path.join(directory, f"{i:02}", "anchor.png"), (anchor_patch[0] * 255).astype(np.uint8))
+        cv.imwrite(os.path.join(directory, f"{i:02}", "positive.png"), (positive_patch[0] * 255).astype(np.uint8))
         if garbage_available:
-            cv.imwrite(os.path.join(dir, f"{i:02}", "garbage.png"), garbage_patch[0])
+            cv.imwrite(os.path.join(directory, f"{i:02}", "garbage.png"), garbage_patch[0])
+
+
+if __name__ == "__main__":
+    main()
